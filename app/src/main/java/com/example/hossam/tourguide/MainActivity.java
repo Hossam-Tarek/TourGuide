4fef376edabc887f50f9b2b297fd90ce10a392aa
Hossam Tarek <hossamtarek250@gmail.com>
2018-03-14 14:05:36 +0200

    Add tabs

--- app/src/main/java/com/example/hossam/tourguide/MainActivity.java
+++ app/src/main/java/com/example/hossam/tourguide/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hossam.tourguide;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
 
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
     }
 }
