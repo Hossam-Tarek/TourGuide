c463f1388544cbd061d6767d57ce3b769b96dbaa
Hossam Tarek <hossamtarek250@gmail.com>
2018-03-14 17:28:14 +0200

    Add content to details screen

--- app/src/main/java/com/example/hossam/tourguide/DetailsActivity.java
+++ app/src/main/java/com/example/hossam/tourguide/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.example.hossam.tourguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        ImageView imageView = findViewById(R.id.detail_image);
+        imageView.setImageResource(getIntent().getExtras().getInt("image_resource_id"));
+
+        TextView name = findViewById(R.id.name);
+        name.setText(getIntent().getExtras().getInt("name_resource_id"));
+
+        TextView description = findViewById(R.id.description);
+        description.setText(getIntent().getExtras().getInt("description_resource_id"));
     }
 }
