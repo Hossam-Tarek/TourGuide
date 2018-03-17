d345743d3b3fd7279f45ca11a14ee08a8cdeb4ae
Hossam Tarek <hossamtarek250@gmail.com>
2018-03-17 09:01:35 +0200

    Add new button to goto location in map

--- app/src/main/java/com/example/hossam/tourguide/DetailsActivity.java
+++ app/src/main/java/com/example/hossam/tourguide/DetailsActivity.java
@@ -1,11 +1,17 @@
 package com.example.hossam.tourguide;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity
+{
+    private String geoLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +22,27 @@ public class DetailsActivity extends AppCompatActivity {
         imageView.setImageResource(getIntent().getExtras().getInt("image_resource_id"));
 
         TextView name = findViewById(R.id.name);
-        name.setText(getIntent().getExtras().getInt("name_resource_id"));
+        String placeName = getString(getIntent().getExtras().getInt("name_resource_id"));
+        name.setText(placeName);
 
         TextView description = findViewById(R.id.description);
         description.setText(getIntent().getExtras().getInt("description_resource_id"));
+
+        Location location = new Location(getIntent().getExtras().getDouble("latitude"),
+                getIntent().getExtras().getDouble("longitude"));
+
+        geoLocation = "geo:0,0?q=" + location.getLatitude() + "," + location.getLongitude() + "(" + placeName + ")";
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(geoLocation));
+
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivity(intent);
+            }
+        });
     }
 }
