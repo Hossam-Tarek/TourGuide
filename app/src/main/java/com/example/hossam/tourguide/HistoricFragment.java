7af1cdaf817f726324bb2bac3651c9e12a31600f
Hossam Tarek <hossamtarek250@gmail.com>
2018-03-17 07:26:13 +0200

    Remove unused imports and strings

--- app/src/main/java/com/example/hossam/tourguide/HistoricFragment.java
+++ app/src/main/java/com/example/hossam/tourguide/HistoricFragment.java
@@ -1,16 +1,12 @@
 package com.example.hossam.tourguide;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
